9f80a2d7a220caa41db09040555a47287296e707
Shivam Tiwari <154796935+Synoep@users.noreply.github.com>
2025-03-01 15:11:19 +0530

    Time: 0 ms (100%), Space: 13.2 MB (62.06%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -8,11 +8,14 @@ public:
                 nums[i+1]=0;
             }
         }
-        vector<int>v;
-        for(int i=0;i<n;i++){
-             if(nums[i]!=0) v.push_back(nums[i]);
+        int index=0;
+         for(int i=0;i<n;i++){
+             if(nums[i]!=0) {
+                swap(nums[i],nums[index]);
+               index++;
+             }
         }
-        while(v.size()<n) v.push_back(0);
-    return v;
+      
+    return nums;
     }
 };
