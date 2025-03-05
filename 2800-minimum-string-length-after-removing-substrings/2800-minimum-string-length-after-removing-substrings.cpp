2c423d69d3f83719acd39d94f29a487c4f7875be
Shivam Tiwari <154796935+Synoep@users.noreply.github.com>
2025-03-05 23:30:43 +0530

    Time: 91 ms (5.24%), Space: 9.9 MB (99.73%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minLength(string s) {
+        int pos1 = s.find("AB"), pos2 = s.find("CD");
+
+        while ((pos1 != string::npos) || (pos2 != string::npos)) {
+            if (pos1 != string::npos) {
+                s.erase(pos1, 2);
+            } else if (pos2 != string::npos) {
+                s.erase(pos2, 2);
+            }
+
+            cout << "Inside Loop : " << s << endl;
+
+            pos1 = s.find("AB");
+            pos2 = s.find("CD");
+        }
+
+        return s.length();
+    }
+};
