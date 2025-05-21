29d8c40d070b8e67f0186c159dd06fa8cf68251c
Shivam Tiwari <154796935+Synoep@users.noreply.github.com>
2025-05-21 13:31:02 +0530

    Time: 2 ms (18.7%), Space: 9.4 MB (8.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
-    int d(int m, int n,vector<vector<int>> &dp){
-        // memo
-        if(m==0 && n==0) return 1;
-        if(m<0 || n<0) return 0;
-        if(dp[m][n]!=-1) return dp[m][n];
-        int up=d(m-1,n,dp);
-        int l=d(m,n-1,dp);
-        return dp[m][n]=l+up;
-    }
+    // int d(int m, int n,vector<vector<int>> &dp){
+    //     // memo
+    //     if(m==0 && n==0) return 1;
+    //     if(m<0 || n<0) return 0;
+    //     if(dp[m][n]!=-1) return dp[m][n];
+    //     int up=d(m-1,n,dp);
+    //     int l=d(m,n-1,dp);
+    //     return dp[m][n]=l+up;
+    // }
     int uniquePaths(int m, int n) {
+        //tabulation
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return d(m-1,n-1,dp);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=1;
+                else {
+                    int up=0,l=0;
+                    if(i>0)up=dp[i-1][j];
+                    if(j>0)l=dp[i][j-1];
+                    dp[i][j]=up+l;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
