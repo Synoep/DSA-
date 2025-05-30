2fc812a93965e495a7b37a16305c67133c7023dc
Shivam Tiwari <154796935+Synoep@users.noreply.github.com>
2025-05-30 21:19:39 +0530

    Time: 0 ms (100%), Space: 14.1 MB (79.07%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
-    int d(int n,int ind,int prev,vector<int>& nums,vector<vector<int>> &dp){
-        if(ind>n-1) return 0;
-        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
-        int ntake=d(n,ind+1,prev,nums,dp);
-        int take=0;
-        if(prev==-1 || nums[ind]>nums[prev] ) take=1+d(n,ind+1,ind,nums,dp);
-        dp[ind][prev+1]=max(ntake,take);
-        return dp[ind][prev+1];
+    // int d(int n,int ind,int prev,vector<int>& nums,vector<vector<int>> &dp){
+    //     if(ind>n-1) return 0;
+    //     if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
+    //     int ntake=d(n,ind+1,prev,nums,dp);
+    //     int take=0;
+    //     if(prev==-1 || nums[ind]>nums[prev] ) take=1+d(n,ind+1,ind,nums,dp);
+    //     dp[ind][prev+1]=max(ntake,take);
+    //     return dp[ind][prev+1];
 
 
-    }
+    // }
     int lengthOfLIS(vector<int>& nums) {
+        // BS
         int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return d(n,0,-1,nums,dp);
+        vector<int>temp;
+        temp.push_back(nums[0]);
+        for(int i=0;i<n;i++){
+            if(nums[i]>temp.back()) temp.push_back(nums[i]);
+            else{
+                int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[ind]=nums[i];
+            }
+        }
+        return temp.size();
     }
 };
