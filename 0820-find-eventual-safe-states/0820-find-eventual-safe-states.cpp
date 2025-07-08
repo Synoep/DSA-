65b569a0fdcf71ba06774ff4bd4935487a031fd2
Shivam Tiwari <154796935+Synoep@users.noreply.github.com>
2025-07-08 21:49:44 +0530

    Time: 7 ms (83.68%), Space: 53.1 MB (64.77%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool dfs(int src, vector<vector<int>> &adj, vector<int> &vis,vector<int> &df,vector<int> &check) {
+        vis[src]=1;
+        df[src]=1;
+            for(auto a: adj[src]) {
+                if(!vis[a]){
+                    if(dfs(a,adj,vis,df,check)==true){
+                         check[src]=0;
+                         return true;
+                    }
+                }
+                else if(df[a]) {
+                    check[src]=0;
+                    return true;
+                }
+            }
+            df[src]=0;
+            check[src]=1;
+            return false;
+        }
+    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
+        int V=adj.size();
+        vector<int> vis(V, 0);
+        vector<int>df(V,0);
+        vector<int>check(V,0);
+        for(int i=0;i<V;i++) {
+            if(!vis[i]) {
+                if(dfs(i, adj, vis,df,check));
+            }
+        }
+        vector<int> ans;
+        for(int i=0;i<V;i++){
+            if(check[i]==1)ans.push_back(i);
+        }
+       return ans;
+    }
+};
