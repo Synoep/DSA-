56586f1e25dac79fda41a3a3a1582029b2263868
Shivam Tiwari <154796935+Synoep@users.noreply.github.com>
2025-07-09 01:06:27 +0530

    Time: 0 ms (100%), Space: 16.4 MB (99.57%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>>& grid, int i, int j, int time) {
+        int n = grid.size(), m = grid[0].size();
+        if (i<0||j<0||i>=n||j>=m) return;
+        if (grid[i][j]==0||(grid[i][j] != 1&&grid[i][j]<time)) return;
+        grid[i][j]=time;
+        dfs(grid, i+1,j,time+1);
+        dfs(grid,i-1,j,time+1);
+        dfs(grid,i,j+1,time+1);
+        dfs(grid,i,j-1,time+1);
+
+    }
     int orangesRotting(vector<vector<int>>& grid) {
-      if(grid.empty()) return 0;
-        int m = grid.size(), n = grid[0].size(), days = 0, tot = 0, cnt = 0;
-        queue<pair<int, int>> rotten;
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                if(grid[i][j] != 0) tot++;
-                if(grid[i][j] == 2) rotten.push({i, j});
-            }
-        }
-        
-        int dx[4] = {0, 0, 1, -1};
-        int dy[4] = {1, -1, 0, 0};
-        
-        while(!rotten.empty()){
-            int k = rotten.size();
-            cnt += k; 
-            while(k--){
-                int x = rotten.front().first, y = rotten.front().second;
-                rotten.pop();
-                for(int i = 0; i < 4; ++i){
-                    int nx = x + dx[i], ny = y + dy[i];
-                    if(nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != 1) continue;
-                    grid[nx][ny] = 2;
-                    rotten.push({nx, ny});
+        int n = grid.size(),m=grid[0].size();
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<m;j++) {
+                if (grid[i][j]==2) {
+                    dfs(grid, i,j,2);
                 }
             }
-            if(!rotten.empty()) days++;
         }
-        
-        return tot == cnt ? days : -1;
+        int maxi=0;
+        for (auto& row:grid) {
+            for (int cell :row) {
+                if (cell==1) return -1; 
+                maxi= max(maxi,cell);
+            }
+        }
+        if(maxi>2) return maxi-2;
+        return 0;
     }
 };
